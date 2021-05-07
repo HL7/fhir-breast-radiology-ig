9f78accc9fa22132b5c28c68a2eff117c23cf34e
Kurt Allen <KurtA@Gjoll.com>
2021-05-07 11:53:17 -0500

    Added fsh-generated

--- Update.bat
+++ Update.bat
@@ -1,9 +1,13 @@
+rmdir /s /q fsh-generated
 rmdir /s /q input
 rmdir /s /q template
 rmdir /s /q txCache
 
 del ig.ini
 del package-list.json
+
+mkdir .\fsh-generated
+xcopy /Q /E /Y ..\BreastRadiologyProfilesV3\Projects\IG\fsh-generated .\fsh-generated
 
 mkdir .\input
 xcopy /Q /E /Y ..\BreastRadiologyProfilesV3\Projects\IG\input .\input
